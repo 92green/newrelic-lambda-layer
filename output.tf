78c9ed3c86a16a503d8af4652fecef4d7482bd5d
David Ramirez <david.ramirez@blueflag.com.au>
2024-02-15 09:17:20 +0800

    fix: removed node_arm_lambda_layer

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
 output "node_lambda_layer" {
   value = local.node_lambda_layer[0]["LatestMatchingVersion"]["LayerVersionArn"]
 }
-
-output "node_arm_lambda_layer" {
-  value = local.node_arm_lambda_layer[0]["LatestMatchingVersion"]["LayerVersionArn"]
-}
