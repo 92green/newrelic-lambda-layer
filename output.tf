e09bfbcb6b207d713e5082ba6aedad7fe2ded395
David Ramirez <david.ramirez@blueflag.com.au>
2024-02-29 07:26:23 +0800

    feat: update to newrelic version 62

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "node_lambda_layer" {
-  value = "arn:aws:lambda:ap-southeast-2:451483290750:layer:NewRelicNodeJS18X:60"
+  value = "arn:aws:lambda:ap-southeast-2:451483290750:layer:NewRelicNodeJS18X:62"
 }
 
 output "node_arm_lambda_layer" {
-  value = "arn:aws:lambda:ap-southeast-2:451483290750:layer:NewRelicNodeJS18XARM64:60"
+  value = "arn:aws:lambda:ap-southeast-2:451483290750:layer:NewRelicNodeJS18XARM64:62"
 }
